ce32e78fea49b7315dbc66dab42023bc62f691cf
SLboat <slboat@me.com>
2013-11-09 04:53:39 +0000

    little text mod

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ IMAGE_BUILD_PATH=OpenWrt-ImageBuilder-ar71xx_generic-for-linux-i486
 # check if have the image build
 if [ ! -d $IMAGE_BUILD_PATH ];then
 	# oh we dont have feeds here:(
-	echo "can't found any image builder in here,wont work anyway:("
-	echo "you may need try [./Get_imagebuild] first!"
+	echo -e "\n\ncan't found any image builder in here,wont work anyway:("
+	echo "you may need try [./get_source.sh] first!"
 	echo -e "plz try again!\n"
 	exit 1
 fi
