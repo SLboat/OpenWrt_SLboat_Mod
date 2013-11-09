de9883148a7ebe9641260344454d78469bb9a52a
SLboat <slboat@me.com>
2013-11-09 08:27:52 +0000

    make the send to wed work well

--- send_web.sh
+++ send_web.sh
@@ -1,6 +1,22 @@
-LOCALIP=$(curl ifconfig.me)
+# check if have the bin path
+if [ ! -d bin/ ];then
+	echo -e "\n\nhave nothing to send,try [./build.sh]first...\n\n"
+	exit 1
+fi
+
+# get the local ip address
+LOCALIP=$(curl icanhazip.com 2>/dev/null)
 cd bin/
-python -m SimpleHTTPServer&
 echo -e "\n\n\n"
-echo "the SimpleHTTPServer& has start..."
-echo "try visit:$(LOCALIP):8000"
+# kill old server,if have one.
+prev_server_pid=$(ps -ef |grep SimpleHTTPServer |grep -v grep | awk '{print $2}')
+if [ -z $prev_server_pid ];then
+	echo -e "Kill the prev server first...\n"
+	kill -9 $prev_server
+fi
+
+# just start the server
+python -m SimpleHTTPServer&
+echo -e "The SimpleHTTPServer has start..." 
+echo "you can visit: http://${LOCALIP}:8000"
+echo -e "\n\n\n"
